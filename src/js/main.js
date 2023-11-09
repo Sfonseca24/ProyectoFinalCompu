c0485e39e2dc9115aa96b0ea653a02406e4e6557
Sfonseca24 <santiagofg246@gmail.com>
2023-11-09 18:17:16 -0500

    creacion de validacion de tipo de archivo

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,22 @@
 function validarExt(){
     var archivoInput = document.getElementById('archivoInput');
     var archivoRuta = archivoInput.value;
-    //var extCorrectas = /(.obj)$/i;
+    var extCorrectas = /(.PNG|.png|.JPG|.jpg)$/i;
 
-    if (archivoInput.files && archivoInput.files[0]){
-        var ver = new FileReader();
-        ver.onload=function(e){
-            document.getElementById('visorArchivo').innerHTML = '<embed src="'+e.target.result+'"width= "200" height= "200">';
-
-        };
-        ver.readAsDataURL(archivoInput.files[0]);
+    if(!extCorrectas.exec(archivoRuta)){
+        alert('Solamente puede previsualizar arhivos .PNG o .JPG')
+        archivoInput.value='';
+        return false;
+    }else{
+        if (archivoInput.files && archivoInput.files[0]){
+            var ver = new FileReader();
+            ver.onload=function(e){
+                document.getElementById('visorArchivo').innerHTML = '<embed src="'+e.target.result+'"width= "200" height= "200">';
+    
+            };
+            ver.readAsDataURL(archivoInput.files[0]);
+        }
     }
+
+    
 }
