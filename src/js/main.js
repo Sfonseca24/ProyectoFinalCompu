0e132d759c035d53e4e83f279832ea8e1952850d
Sfonseca24 <santiagofg246@gmail.com>
2023-11-22 04:31:44 -0500

    quitando y poniendo cosas

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,11 @@ var scene = null,
     path = null,
     path2 = null,
     Ggltf = null,
-    loader= null;
+    loader= null,
+    gridHelper,
+    axesHelper,
+    lightAmbient,
+    light;
 
 const size = 20,
     divisions = 20;
@@ -14,7 +18,7 @@ const size = 20,
     function startScene() {
         // Scene, Camera, Renderer
         scene = new THREE.Scene();
-        scene.background = new THREE.Color(0x828282); //33FFC5
+        scene.background = new THREE.Color(0xbfd3aa); //33FFC5
         camera = new THREE.PerspectiveCamera(
             75,                                        //Angulo de visión(Abajo o arriba) 
             window.innerWidth / window.innerHeight,    //Relación de aspecto 16:9
@@ -36,20 +40,18 @@ const size = 20,
     
     
         //Grid Helper
-        const gridHelper = new THREE.GridHelper(size, divisions);
-        scene.add(gridHelper);
+        gridHelper = new THREE.GridHelper(size, divisions, 0x000);
+        
     
         //Axes Helper
-        // const axesHelper = new THREE.AxesHelper(5);
-        // scene.add(axesHelper);
+        axesHelper = new THREE.AxesHelper(5);
+        scene.add(axesHelper);
     
-        const lightAmbient = new THREE.AmbientLight(0xfffff); // soft white light
-        scene.add(lightAmbient);
+        lightAmbient = new THREE.AmbientLight(0xFFFFFF); // soft white light
+        
     
-        const light = new THREE.PointLight( 0xffffff, 1, 100 );
-        light.position.set( 5,10,10 );
-        scene.add( light );
-    
+        light = new THREE.PointLight( 0xffffff, 1, 100 );
+        
     
     
     
@@ -199,35 +201,71 @@ function CambiarVista(key){
     
 }
 
-function CambiarCaracteristicas(option){
-    switch (option) {
-        case "mesh":
-            const material = new THREE.MeshBasicMaterial({wireframe: true, color: 0X00ff00});
-            gltf = new THREE.Mesh(material);
-            path.style.color = "transparent";
-            break;
-            case value:
-            
-            break;
-            case value:
-            
-            break;
-            case value:
-            
-            break;
-    
-        default:
-            break;
-    }
-}
+// function CambiarCaracteristicas(option){
+//     switch (option) {
+//         case "mesh":
+//             light.position.set( 5,10,10 );
+//         scene.add( light );
+//             break;
+//         case "gridHelper":
+//                 scene.add(gridHelper);
+//             break;
+          
+//     }
+// }
 
-function Animar(key){
-    switch (key) {
-        case "rotar":
-            Ggltf.rotation.x +=5;
-            break;
+function Añadirquitar(option){
     
-        default:
+    switch (option) {
+        case "light":
+            var luz = document.getElementById('flexCheckDefault');
+            if (luz.value=="false"){
+                light.position.set( 5,10,10 );
+                scene.add( light );
+                luz.value="true";
+            }else if(luz.value=="true"){
+                scene.remove(light);
+                luz.value="false";
+            }
+            
             break;
+            case "gridHelper":
+                var grid = document.getElementById('flexCheckDefault2');
+            if (grid.value=="true"){
+                
+                scene.add(gridHelper);
+                grid.value = "false";
+            }else if(grid.value=="false"){
+                scene.remove(gridHelper);
+                grid.value = "true";
+            }
+                
+            break;
+            case "lightAmbient":
+                var ambient = document.getElementById('flexCheckDefault3');
+            if (ambient.value=="true"){
+                
+                scene.add(lightAmbient);
+                ambient.value = "false";
+            }else if(ambient.value=="false"){
+                scene.remove(lightAmbient);
+                ambient.value = "true";
+            }
+                
+            break;
+
+            case "axesHelper":
+                var axes = document.getElementById('flexCheckDefault4');
+            if (axes.value=="true"){
+                
+                scene.add(axesHelper);
+                axes.value = "false";
+            }else if(axes.value=="false"){
+                scene.remove(axesHelper);
+                axes.value = "true";
+            }
+                
+            break;
+           
     }
 }
