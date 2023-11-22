5064a06585dd4ff370f24ed900c5abba30ab14e5
Sfonseca24 <santiagofg246@gmail.com>
2023-11-22 03:25:38 -0500

    yo creo que ya

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,9 @@ var scene = null,
     controls = null,
     prueba = null,
     path = null,
-    path2 = null;
+    path2 = null,
+    Ggltf = null,
+    loader= null;
 
 const size = 20,
     divisions = 20;
@@ -12,13 +14,16 @@ const size = 20,
     function startScene() {
         // Scene, Camera, Renderer
         scene = new THREE.Scene();
-        scene.background = new THREE.Color(0x33FFC5); //33FFC5
+        scene.background = new THREE.Color(0x828282); //33FFC5
         camera = new THREE.PerspectiveCamera(
             75,                                        //Angulo de visión(Abajo o arriba) 
             window.innerWidth / window.innerHeight,    //Relación de aspecto 16:9
             0.1,                                       //Mas cerca (no renderiza)
             1000);                                    //Mas lejos ()
-    
+            
+
+        
+            
         renderer = new THREE.WebGLRenderer({ canvas: document.getElementById('cargarModels') });
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body.appendChild(renderer.domElement);
@@ -65,6 +70,7 @@ const size = 20,
         controls.update();
         renderer.render(scene, camera);
         // console.log(camera.position);
+
         
         
     
@@ -85,7 +91,7 @@ const size = 20,
 
 function loadDuck_Gltf(path) {
     // Instantiate a loader DUCK
-    const loader = new THREE.GLTFLoader();
+    loader = new THREE.GLTFLoader();
 
     // // Optional: Provide a DRACOLoader instance to decode compressed mesh data
     // const dracoLoader = new THREE.DRACOLoader();
@@ -100,6 +106,8 @@ function loadDuck_Gltf(path) {
         function (gltf) {
 
             scene.add(gltf.scene);
+
+            Ggltf = gltf;
 
             gltf.animations; // Array<THREE.AnimationClip>
             gltf.scene.position.set(0, 1, 0);
@@ -143,6 +151,7 @@ function Upload3d() {
     if (archivoInput.files && archivoInput.files[0]) {
         var ver = new FileReader();
         ver.onload = function (e) {
+        
             path = e.target.result;
 
             // console.log("path: "+path);
@@ -164,4 +173,61 @@ function Upload3d() {
         // console.log("este es path final: "+path);
     }
     }    
+}
+
+function CambiarVista(key){
+    switch (key) {
+        case "arriba":
+            camera.position.set(0,13,0);
+            break;
+        case "abajo":
+            camera.position.set(0,-13,0);
+            break;
+        case "frontal":
+            camera.position.set(0,0,13);
+            break;
+        case "lateral":
+            camera.position.set(14,0,0);
+            break;
+        case "perspectiva":
+            camera.position.set(8, 7, 8);
+            break;
+        default:
+            alert("esta vista no exite");
+            break;
+    }
+    
+}
+
+function CambiarCaracteristicas(option){
+    switch (option) {
+        case "mesh":
+            const material = new THREE.MeshBasicMaterial({wireframe: true, color: 0X00ff00});
+            gltf = new THREE.Mesh(material);
+            path.style.color = "transparent";
+            break;
+            case value:
+            
+            break;
+            case value:
+            
+            break;
+            case value:
+            
+            break;
+    
+        default:
+            break;
+    }
+}
+
+function Animar(key){
+    switch (key) {
+        case "rotar":
+            Ggltf.rotation.x +=5;
+            break;
+    
+        default:
+            break;
+    }
 }
